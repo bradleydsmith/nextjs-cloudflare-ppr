19e5339317260afcc4831193139945580c56a26f
Bradley Smith <bradleysmith93@gmail.com>
2025-01-21 10:15:11 +1100

    style: clean up styling of code in cf-worker

--- cf-worker/src/build.js
+++ cf-worker/src/build.js
@@ -1,8 +1,10 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const nextDirectory = `${__dirname}/../..`;
 
-const prerenderManifest = JSON.parse(fs.readFileSync(`${nextDirectory}/.next/prerender-manifest.json`, 'utf8'));
+const prerenderManifest = JSON.parse(
+  fs.readFileSync(`${nextDirectory}/.next/prerender-manifest.json`, "utf8"),
+);
 
 // Combine both static and dynamic routes
 let routes = [
@@ -10,20 +12,22 @@ let routes = [
   ...Object.entries(prerenderManifest.routes).map(([route, data]) => ({
     route,
     dynamic: false,
-    ...data
+    ...data,
   })),
   // Dynamic routes
   ...Object.entries(prerenderManifest.dynamicRoutes).map(([route, data]) => ({
     route: data.fallback,
     routeRegex: data.routeRegex,
     dynamic: true,
-    ...data
-  }))
+    ...data,
+  })),
 ];
 
 // Delete any routes that aren't PARTIALLY_STATIC with experimentalPPR
-routes = routes.filter(route => 
-  route.renderingMode === 'PARTIALLY_STATIC' && route.experimentalPPR === true
+routes = routes.filter(
+  (route) =>
+    route.renderingMode === "PARTIALLY_STATIC" &&
+    route.experimentalPPR === true,
 );
 
 // Delete and recreate prerender directory
@@ -41,11 +45,11 @@ let routeMappings = [];
 
 for (const routeObj of routes) {
   // Split route into path segments and remove empty strings
-  const segments = routeObj.route.split('/').filter(Boolean);
-  
+  const segments = routeObj.route.split("/").filter(Boolean);
+
   // Don't create directory for the last segment since it will be a file
   const dirSegments = segments.slice(0, -1);
-  
+
   // Build up the directory path
   let currentPath = `${__dirname}/prerender`;
   for (const segment of dirSegments) {
@@ -56,11 +60,22 @@ for (const routeObj of routes) {
     }
   }
 
-  const routePath = routeObj.route === '/' ? 'index' : routeObj.route;
-  
-  const html = fs.readFileSync(`${nextDirectory}/.next/server/app/${routePath}.html`, 'utf8');
-  const rsc = fs.readFileSync(`${nextDirectory}/.next/server/app/${routePath}.prefetch.rsc`, 'utf8');
-  const meta = JSON.parse(fs.readFileSync(`${nextDirectory}/.next/server/app/${routePath}.meta`, 'utf8'));
+  const routePath = routeObj.route === "/" ? "index" : routeObj.route;
+
+  const html = fs.readFileSync(
+    `${nextDirectory}/.next/server/app/${routePath}.html`,
+    "utf8",
+  );
+  const rsc = fs.readFileSync(
+    `${nextDirectory}/.next/server/app/${routePath}.prefetch.rsc`,
+    "utf8",
+  );
+  const meta = JSON.parse(
+    fs.readFileSync(
+      `${nextDirectory}/.next/server/app/${routePath}.meta`,
+      "utf8",
+    ),
+  );
 
   let postponed = false;
   if (meta.postponed) {
@@ -71,7 +86,7 @@ for (const routeObj of routes) {
     html,
     rsc,
     postponed,
-    dynamic: routeObj.dynamic
+    dynamic: routeObj.dynamic,
   };
 
   // Add routeRegex if it exists
@@ -79,23 +94,27 @@ for (const routeObj of routes) {
     data.routeRegex = routeObj.routeRegex;
   }
 
-  fs.writeFileSync(`${__dirname}/prerender/${routePath}.json`, JSON.stringify(data, null, 2));
+  fs.writeFileSync(
+    `${__dirname}/prerender/${routePath}.json`,
+    JSON.stringify(data, null, 2),
+  );
 
   // Add mapping to the array
   routeMappings.push({
     route: routeObj.route,
     dynamic: routeObj.dynamic,
     routeRegex: routeObj.routeRegex,
-    jsonPath: routeObj.route === '/' 
-      ? './prerender/index.json'
-      : `./prerender${routeObj.route}.json`
+    jsonPath:
+      routeObj.route === "/"
+        ? "./prerender/index.json"
+        : `./prerender${routeObj.route}.json`,
   });
 }
 
 // Write the routes.json file
 fs.writeFileSync(
-  `${__dirname}/prerender/routes.json`, 
-  JSON.stringify(routeMappings, null, 2)
+  `${__dirname}/prerender/routes.json`,
+  JSON.stringify(routeMappings, null, 2),
 );
 
 // Check if the ../public/_next directory exists, if so delete it and recreate it. Then copy
@@ -111,6 +130,10 @@ if (fs.existsSync(`${__dirname}/../public/_next`)) {
   fs.rmdirSync(`${__dirname}/../public/_next`, { recursive: true });
 }
 fs.mkdirSync(`${__dirname}/../public/_next`);
-fs.cpSync(`${nextDirectory}/.next/static`, `${__dirname}/../public/_next/static`, { recursive: true });
+fs.cpSync(
+  `${nextDirectory}/.next/static`,
+  `${__dirname}/../public/_next/static`,
+  { recursive: true },
+);
 
-console.log('JSON files and routes mapping created successfully!');
+console.log("JSON files and routes mapping created successfully!");
--- cf-worker/src/index.js
+++ cf-worker/src/index.js
@@ -1,209 +1,222 @@
-const BASE_ORIGIN = 'http://origin.yourserverhere.com';
+const BASE_ORIGIN = "http://origin.yourserverhere.com";
 
 export default {
-	async fetch(request, env, ctx) {
-		const url = new URL(request.url);
-		
-		try {
-			const routesText = await import('./prerender/routes.json');
-			const routes = JSON.parse(routesText.default);
-			// Check if the pathname matches any route in routes.json
-			const route = routes.find(r => 
-				(!r.dynamic && r.route === url.pathname) || 
-				(r.dynamic && new RegExp(r.routeRegex).test(url.pathname))
-			);
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
 
-      		const isRSC = request.headers.get('rsc') === '1';
-      		const isNextStateTree = request.headers.get('next-router-state-tree') != null ? true : false;
-			const isNextRouterPrefetch = request.headers.get('next-router-prefetch') === '1' ? true : false;
+    try {
+      const routesText = await import("./prerender/routes.json");
+      const routes = JSON.parse(routesText.default);
+      // Check if the pathname matches any route in routes.json
+      const route = routes.find(
+        (r) =>
+          (!r.dynamic && r.route === url.pathname) ||
+          (r.dynamic && new RegExp(r.routeRegex).test(url.pathname)),
+      );
 
-			if (request.method === 'GET' && (isNextRouterPrefetch || !isNextStateTree) && route) {
-        		if (isRSC) {
-          			return await handleRSCPath(request, route.jsonPath);
-        		} else {
-          			return await handleHtmlPath(request, route.jsonPath);
-        		}
-      		} else if (request.method === 'GET' && isNextStateTree) {
-      			return await handleNextStateTree(request);
-      		} else {
-				return await handleProxyRequest(request);
-			}
-		} catch (error) {
-			console.error('Error handling request:', error);
-			return new Response('Internal Server Error', { status: 500 });
-		}
-	},
+      const isRSC = request.headers.get("rsc") === "1";
+      const isNextStateTree =
+        request.headers.get("next-router-state-tree") != null ? true : false;
+      const isNextRouterPrefetch =
+        request.headers.get("next-router-prefetch") === "1" ? true : false;
+
+      if (
+        request.method === "GET" &&
+        (isNextRouterPrefetch || !isNextStateTree) &&
+        route
+      ) {
+        if (isRSC) {
+          return await handleRSCPath(request, route.jsonPath);
+        } else {
+          return await handleHtmlPath(request, route.jsonPath);
+        }
+      } else if (request.method === "GET" && isNextStateTree) {
+        return await handleNextStateTree(request);
+      } else {
+        return await handleProxyRequest(request);
+      }
+    } catch (error) {
+      console.error("Error handling request:", error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
+  },
 };
 
 async function handleHtmlPath(req, jsonPath) {
-	const routeDataText = await import(jsonPath);
-	const routeData = JSON.parse(routeDataText.default);
-	const htmlContent = routeData.html;
+  const routeDataText = await import(jsonPath);
+  const routeData = JSON.parse(routeDataText.default);
+  const htmlContent = routeData.html;
 
-	const headers = new Headers(req.headers);
-	headers.delete('host');
+  const headers = new Headers(req.headers);
+  headers.delete("host");
 
-	const url = new URL(req.url);
-	const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
+  const url = new URL(req.url);
+  const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
 
-	return new Response(
-		new ReadableStream({
-			async start(controller) {
-				try {
-					controller.enqueue(new TextEncoder().encode(htmlContent));
-					
-					if (routeData.postponed) {
-						const restOfResponse = await fetch(proxyUrl, {
-			              headers
-			            });
-						const reader = restOfResponse.body.getReader();
-						let bytesRead = 0;
-						const htmlLength = new TextEncoder().encode(htmlContent).length;
+  return new Response(
+    new ReadableStream({
+      async start(controller) {
+        try {
+          controller.enqueue(new TextEncoder().encode(htmlContent));
 
-						while (true) {
-							const {done, value} = await reader.read();
-							if (done) break;
+          if (routeData.postponed) {
+            const restOfResponse = await fetch(proxyUrl, {
+              headers,
+            });
+            const reader = restOfResponse.body.getReader();
+            let bytesRead = 0;
+            const htmlLength = new TextEncoder().encode(htmlContent).length;
 
-							if (bytesRead + value.length <= htmlLength) {
-								// Skip this chunk entirely
-								bytesRead += value.length;
-							} else if (bytesRead < htmlLength) {
-								// Partial chunk needs to be skipped
-								const remainingToSkip = htmlLength - bytesRead;
-								controller.enqueue(value.slice(remainingToSkip));
-								bytesRead = htmlLength;
-							} else {
-								// Past the HTML content, enqueue everything
-								controller.enqueue(value);
-							}
-						}
-					}
-				} catch (error) {
-					console.error(error);
-				} finally {
-					controller.close();
-				}
-			}
-		}),
-		{
-			headers: {
-				'Content-Type': 'text/html; charset=utf-8',
-				'Transfer-Encoding': 'chunked',
-				'Cache-Control': 'private, no-cache, no-store, max-age=0, must-revalidate',
-        		'Vary': 'RSC, Next-Router-State-Tree, Next-Router-Prefetch, Next-Router-Segment-Prefetch, Accept-Encoding'
-			},
-		}
-	);
+            while (true) {
+              const { done, value } = await reader.read();
+              if (done) break;
+
+              if (bytesRead + value.length <= htmlLength) {
+                // Skip this chunk entirely
+                bytesRead += value.length;
+              } else if (bytesRead < htmlLength) {
+                // Partial chunk needs to be skipped
+                const remainingToSkip = htmlLength - bytesRead;
+                controller.enqueue(value.slice(remainingToSkip));
+                bytesRead = htmlLength;
+              } else {
+                // Past the HTML content, enqueue everything
+                controller.enqueue(value);
+              }
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          controller.close();
+        }
+      },
+    }),
+    {
+      headers: {
+        "Content-Type": "text/html; charset=utf-8",
+        "Transfer-Encoding": "chunked",
+        "Cache-Control":
+          "private, no-cache, no-store, max-age=0, must-revalidate",
+        Vary: "RSC, Next-Router-State-Tree, Next-Router-Prefetch, Next-Router-Segment-Prefetch, Accept-Encoding",
+      },
+    },
+  );
 }
 
 async function handleRSCPath(req, jsonPath) {
-	const routeDataText = await import(jsonPath);
-	const routeData = JSON.parse(routeDataText.default);
-	const rscContent = routeData.rsc;
+  const routeDataText = await import(jsonPath);
+  const routeData = JSON.parse(routeDataText.default);
+  const rscContent = routeData.rsc;
 
-	// Get the last two lines of rscContent
-	const lines = rscContent.split('\n');
-	const lastTwoLines = lines.slice(-2).join('\n');
+  // Get the last two lines of rscContent
+  const lines = rscContent.split("\n");
+  const lastTwoLines = lines.slice(-2).join("\n");
 
-	const headers = new Headers(req.headers);
-	headers.delete('host');
+  const headers = new Headers(req.headers);
+  headers.delete("host");
 
-	const url = new URL(req.url);
-	const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
+  const url = new URL(req.url);
+  const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
 
-	return new Response(
-		new ReadableStream({
-			async start(controller) {
-				try {
-					controller.enqueue(new TextEncoder().encode(rscContent));
+  return new Response(
+    new ReadableStream({
+      async start(controller) {
+        try {
+          controller.enqueue(new TextEncoder().encode(rscContent));
 
-					const nextRouterPrefetch = req.headers.get('next-router-prefetch') ? true : false;
-					
-					if (routeData.postponed && !nextRouterPrefetch) {
-						const restOfResponse = await fetch(proxyUrl, { headers });
-						const reader = restOfResponse.body.getReader();
-						
-						let buffer = '';
-						const decoder = new TextDecoder();
+          const nextRouterPrefetch = req.headers.get("next-router-prefetch")
+            ? true
+            : false;
 
-						while (true) {
-							const {done, value} = await reader.read();
-							if (done) break;
-							
-							buffer += decoder.decode(value, {stream: true});
-							
-							// Look for the marker (last two lines) in the buffer
-							const markerIndex = buffer.indexOf(lastTwoLines);
-							if (markerIndex !== -1) {
-								// Found the marker, skip everything before it (including the marker)
-								buffer = buffer.slice(markerIndex + lastTwoLines.length);
-								// Start enqueueing from this point forward
-								if (buffer.length > 0) {
-									controller.enqueue(new TextEncoder().encode(buffer));
-								}
-								// Direct streaming for remaining chunks
-								while (true) {
-									const {done, value} = await reader.read();
-									if (done) break;
-									controller.enqueue(value);
-								}
-								break;
-							}
-						}
-					}
-				} catch (error) {
-					console.error(error);
-				} finally {
-					controller.close();
-				}
-			}
-		}),
-		{
-			headers: {
-				'Content-Type': 'text/x-component',
-				'Transfer-Encoding': 'chunked',
-				'Cache-Control': 'private, no-cache, no-store, max-age=0, must-revalidate',
-				'Vary': 'RSC, Next-Router-State-Tree, Next-Router-Prefetch, Next-Router-Segment-Prefetch, Accept-Encoding',
-				'X-Nextjs-Postponed': routeData.postponed ? '1' : undefined,
-				'X-Nextjs-Prerender': 1
-			},
-		}
-	);
+          if (routeData.postponed && !nextRouterPrefetch) {
+            const restOfResponse = await fetch(proxyUrl, { headers });
+            const reader = restOfResponse.body.getReader();
+
+            let buffer = "";
+            const decoder = new TextDecoder();
+
+            while (true) {
+              const { done, value } = await reader.read();
+              if (done) break;
+
+              buffer += decoder.decode(value, { stream: true });
+
+              // Look for the marker (last two lines) in the buffer
+              const markerIndex = buffer.indexOf(lastTwoLines);
+              if (markerIndex !== -1) {
+                // Found the marker, skip everything before it (including the marker)
+                buffer = buffer.slice(markerIndex + lastTwoLines.length);
+                // Start enqueueing from this point forward
+                if (buffer.length > 0) {
+                  controller.enqueue(new TextEncoder().encode(buffer));
+                }
+                // Direct streaming for remaining chunks
+                while (true) {
+                  const { done, value } = await reader.read();
+                  if (done) break;
+                  controller.enqueue(value);
+                }
+                break;
+              }
+            }
+          }
+        } catch (error) {
+          console.error(error);
+        } finally {
+          controller.close();
+        }
+      },
+    }),
+    {
+      headers: {
+        "Content-Type": "text/x-component",
+        "Transfer-Encoding": "chunked",
+        "Cache-Control":
+          "private, no-cache, no-store, max-age=0, must-revalidate",
+        Vary: "RSC, Next-Router-State-Tree, Next-Router-Prefetch, Next-Router-Segment-Prefetch, Accept-Encoding",
+        "X-Nextjs-Postponed": routeData.postponed ? "1" : undefined,
+        "X-Nextjs-Prerender": 1,
+      },
+    },
+  );
 }
 
 async function handleProxyRequest(req) {
-	const url = new URL(req.url);
-	const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
+  const url = new URL(req.url);
+  const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
 
-	const headers = new Headers(req.headers);
-	headers.delete('host');
-	headers.set('origin', BASE_ORIGIN);
+  const headers = new Headers(req.headers);
+  headers.delete("host");
+  headers.set("origin", BASE_ORIGIN);
 
-	return fetch(proxyUrl, {
-		method: req.method,
-		headers,
-		body: req.method !== 'GET' && req.method !== 'HEAD' ? req.body : undefined,
-		cf: { // Cloudflare-specific options
-			cacheTtl: 0,
-			cacheEverything: false,
-		},
-	});
+  return fetch(proxyUrl, {
+    method: req.method,
+    headers,
+    body: req.method !== "GET" && req.method !== "HEAD" ? req.body : undefined,
+    cf: {
+      // Cloudflare-specific options
+      cacheTtl: 0,
+      cacheEverything: false,
+    },
+  });
 }
 
 async function handleNextStateTree(req) {
-    const headers = new Headers(req.headers);
-    headers.delete('host');
+  const headers = new Headers(req.headers);
+  headers.delete("host");
 
-    const url = new URL(req.url);
-    const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
+  const url = new URL(req.url);
+  const proxyUrl = `${BASE_ORIGIN}${url.pathname}${url.search}`;
 
-    const response = await fetch(proxyUrl, { headers });
-    return new Response(response.body, {
-        headers: {
-            'Content-Type': 'text/x-component',
-            'Transfer-Encoding': 'chunked', 
-            'Cache-Control': 'private, no-cache, no-store, max-age=0, must-revalidate',
-            'Vary': 'RSC, Next-Router-State-Tree, Next-Router-Prefetch, Next-Router-Segment-Prefetch, Accept-Encoding'
-        }
-    });
+  const response = await fetch(proxyUrl, { headers });
+  return new Response(response.body, {
+    headers: {
+      "Content-Type": "text/x-component",
+      "Transfer-Encoding": "chunked",
+      "Cache-Control":
+        "private, no-cache, no-store, max-age=0, must-revalidate",
+      Vary: "RSC, Next-Router-State-Tree, Next-Router-Prefetch, Next-Router-Segment-Prefetch, Accept-Encoding",
+    },
+  });
 }
